8b31b0a889a63396325439c6259c130ea1398abb
Matt Lacey <github@mrlacey.co.uk>
2014-10-13 20:39:53 +0100

    Basic Examples

---
+++ AdDuplexSamples/SLWP80/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SLWP80.Resources;
+
+namespace SLWP80
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ AdDuplexSamples/SLWP81/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SLWP81.Resources;
+
+namespace SLWP81
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
