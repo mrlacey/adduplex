7cde057961ec1c1041ff8642bea3ef7ac07975a2
Matt Lacey <github@mrlacey.co.uk>
2015-04-10 10:39:47 +0100

    Added Unity sample
    + updated Win8 version to latest SDK

---
+++ AdDuplexSamples/Unity/AdDuplexUnityDemo/Assets/ButtonActions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonActions : MonoBehaviour {
+	
+	public void ToggleBannerAd () {
+		Interop.ToggleBannerAdControl();
+	}
+	
+	public void ShowInterstitial () {
+		Interop.ShowInterstitialAd(); 
+	}
+}
---
+++ AdDuplexSamples/Unity/AdDuplexUnityDemo/Assets/Interop.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static class Interop {
+	
+	public static event EventHandler ToggleBannerAdVisibility;
+	public static event EventHandler LoadInterstitialEvent;
+	public static event EventHandler ShowInterstitialEvent;
+	public static event EventHandler OnInterstitialClosed;
+	
+	public static void LoadInterstitialAd()
+	{
+		if (LoadInterstitialEvent != null)
+		{
+			LoadInterstitialEvent(null, EventArgs.Empty);
+		}
+	}
+	
+	public static void ShowInterstitialAd()
+	{
+		if (ShowInterstitialEvent != null)
+		{
+			ShowInterstitialEvent(null, EventArgs.Empty);
+		}
+	}
+	
+	public static void InterstitialClose()
+	{
+		if (OnInterstitialClosed != null)
+		{
+			OnInterstitialClosed(null, EventArgs.Empty);		
+		}
+	}
+	
+	public static void ToggleBannerAdControl()
+	{
+		if (ToggleBannerAdVisibility != null)
+		{
+			ToggleBannerAdVisibility(null, null);			
+		}		
+	}	
+}
