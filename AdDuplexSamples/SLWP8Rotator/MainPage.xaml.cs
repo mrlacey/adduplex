01cdd15665949b32883dcde5db3531d4a56ed82c
Matt Lacey <github@mrlacey.co.uk>
2015-04-17 22:59:23 +0100

    SLWP81 via AdRotator

---
+++ AdDuplexSamples/SLWP8Rotator/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using AdRotator;
+using AdRotator.AdProviders;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using SLWP8Rotator.Resources;
+
+namespace SLWP8Rotator
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
