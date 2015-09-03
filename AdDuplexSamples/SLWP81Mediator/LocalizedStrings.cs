bc29580de0ab4af015d15b7b2ded6b1dd373a8ab
Matt Lacey <github@mrlacey.co.uk>
2015-09-03 12:04:56 +0100

    AdMediator samples

---
+++ AdDuplexSamples/SLWP81Mediator/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SLWP81Mediator.Resources;
+
+namespace SLWP81Mediator
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ AdDuplexSamples/SLWP81Mediator/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AdMediator.Core.Events;
+using Microsoft.Phone.Controls;
+
+namespace SLWP81Mediator
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AdMediator_A219F9_OnAdMediatorError(object sender, AdMediatorFailedEventArgs e)
+        {
+            System.Diagnostics.Debugger.Break();
+        }
+
+        private void AdMediator_A219F9_OnAdSdkError(object sender, AdFailedEventArgs e)
+        {
+            System.Diagnostics.Debugger.Break();
+        }
+    }
+}
