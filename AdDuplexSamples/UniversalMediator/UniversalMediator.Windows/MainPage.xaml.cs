cefaba36a39c6e2f7dccf4b6a93d22430c9a450e
Matt Lacey <github@mrlacey.co.uk>
2016-04-28 09:20:09 +0100

    Misc additions

--- AdDuplexSamples/UniversalMediator/UniversalMediator.Windows/MainPage.xaml.cs
+++ AdDuplexSamples/UniversalMediator/UniversalMediator.Windows/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.AdMediator.Core.Models;
+using Microsoft.AdMediator.Core.Utilities;
+using Microsoft.AdMediator.Windows81;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +29,18 @@ namespace UniversalMediator
         public MainPage()
         {
             this.InitializeComponent();
+            
+            this.AdMediator_6E6F90.AdMediatorFilled += AdMediator_6E6F90_AdMediatorFilled;
+        }
+
+        private void AdMediator_6E6F90_AdMediatorFilled(object sender, Microsoft.AdMediator.Core.Events.AdSdkEventArgs e)
+        {
+            // VS may put red underlines here but can ignore them
+             this.AdMediator_6E6F90.AdSdkOptionalParameters[AdSdkNames.AdDuplex]["Size"] = "160×600";
+
+            //this.AdMediator_6E6F90.AdSdkOptionalParameters[AdSdkNames.AdDuplex]["Width"] = 160;
+            //this.AdMediator_6E6F90.AdSdkOptionalParameters[AdSdkNames.AdDuplex]["Height"] = 600;
         }
     }
+
 }
--- AdDuplexSamples/UwpAdMediator/MainPage.xaml.cs
+++ AdDuplexSamples/UwpAdMediator/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.AdMediator.Core.Events;
+using Microsoft.AdMediator.Core.Models;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +28,34 @@ namespace UwpAdMediator
         public MainPage()
         {
             this.InitializeComponent();
+
+
+            desk5.AdSdkOptionalParameters[AdSdkNames.AdDuplex]["IsTest"] = true;
+            // desk5.AdSdkOptionalParameters[AdSdkNames.AdDuplex]["Height"] = 50;
+            //  desk5.AdSdkOptionalParameters[AdSdkNames.AdDuplex]["Width"] = 300;
+           // desk5.AdSdkOptionalParameters[AdSdkNames.AdDuplex]["Height"] = 80;
+           // desk5.AdSdkOptionalParameters[AdSdkNames.AdDuplex]["Width"] = 480;
+
+        }
+
+        private void Desk5_OnAdMediatorError(object sender, AdMediatorFailedEventArgs e)
+        {
+            Debug.WriteLine("Desk5_OnAdMediatorError: " + e.Error);
+}
+
+        private void Desk5_OnAdMediatorFilled(object sender, AdSdkEventArgs e)
+        {
+            Debug.WriteLine("Desk5_OnAdMediatorFilled: " + e.EventName);
+        }
+
+        private void Desk5_OnAdSdkError(object sender, AdFailedEventArgs e)
+        {
+            Debug.WriteLine("Desk5_OnAdSdkError: " + e.Error);
+        }
+
+        private void Desk5_OnAdSdkEvent(object sender, AdSdkEventArgs e)
+        {
+            Debug.WriteLine("Desk5_OnAdSdkEvent: " + e.EventName);
         }
     }
 }
